4720638acbc22964c3bb204e50ee44aa1cd2d5d1
wahyu <wahyusutejo1986@gmail.com>
2024-12-27 18:54:32 +0700

    update opencti port 	modified:   modules/opencti/.env.sample 	modified:   modules/opencti/deploy_opencti.sh 	modified:   modules/opencti/docker-compose.dev.yml 	modified:   modules/opencti/docker-compose.yml

--- modules/opencti/deploy_opencti.sh
+++ modules/opencti/deploy_opencti.sh
@@ -2,16 +2,14 @@
 
 # setting for elasticsearch
 sudo sysctl -w vm.max_map_count=1048575
-vm.max_map_count=1048575
-
 sudo cp .env.sample .env
 
 # add configuration to .env
-sudo (cat << EOF
+$(cat << EOF
 OPENCTI_ADMIN_EMAIL=admin@opencti.io
 OPENCTI_ADMIN_PASSWORD=ChangeMePlease
 OPENCTI_ADMIN_TOKEN=$(cat /proc/sys/kernel/random/uuid)
-OPENCTI_BASE_URL=http://localhost:8080
+OPENCTI_BASE_URL=http://localhost:8282
 MINIO_ROOT_USER=$(cat /proc/sys/kernel/random/uuid)
 MINIO_ROOT_PASSWORD=$(cat /proc/sys/kernel/random/uuid)
 RABBITMQ_DEFAULT_USER=guest
@@ -28,4 +26,4 @@ EOF
 ) > .env
 
 # running openCTI Container
-docker-compose up -d
+sudo docker-compose up -d
