41820df5b6429ef6c22b9ea24005130b6c5d08a2
nobone-test <79396632+nobone-test@users.noreply.github.com>
2024-12-31 23:16:50 +0700

    Update misp.sh
    edited script remove directory mapping

--- modules/misp/misp.sh
+++ modules/misp/misp.sh
@@ -3,7 +3,7 @@
 set -e  # Exit on error
 
 # get directory where this script install_all.sh executed
-# BASE_DIR=$(pwd)
+BASE_DIR=$(pwd)
 
 # Load configuration
 script_dir=$(cd "$(dirname "$0")" && pwd)
@@ -17,24 +17,7 @@ else
     exit 1
 fi
 
-# Create /tmp/socarium directory if it doesn't exist
-SOC_DIR="/tmp/socarium"
-if [ ! -d "$SOC_DIR" ]; then
-    echo "Creating directory $SOC_DIR..."
-    mkdir -p "$SOC_DIR"
-else
-    echo "Directory $SOC_DIR already exists."
-fi
-
-# Copy modules directory to /tmp/socarium if it doesn't already exist
-MODULES_SRC="modules"
-MODULES_DEST="$SOC_DIR/modules"
-if [ ! -d "$MODULES_DEST" ]; then
-    echo "Copying modules to $SOC_DIR..."
-    cp -r "$MODULES_SRC" "$MODULES_DEST"
-else
-    echo "Modules directory already exists in $SOC_DIR. Skipping copy."
-fi
+SOC_DIR="/home/$(logname)/soc"
 
 cd "$SOC_DIR"
 
