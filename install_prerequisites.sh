68ab80f66b2274c54859d4d1b86f22bfd0d59ec5
nobone-test <79396632+nobone-test@users.noreply.github.com>
2025-01-03 09:20:39 +0700

    Update install_prerequisites.sh
    move python-pip

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -14,8 +14,6 @@ echo "=============================="
 # Step 1: Update the system
 echo "Updating system packages..."
 sudo apt update -y
-sudo apt install python3-pip -y
-sudo apt install python3-venv
 
 # Step 2: Install required packages
 echo "Installing required packages..."
