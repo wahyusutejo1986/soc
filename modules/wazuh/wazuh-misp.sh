62371e51e1afedbac4ecd2135daa63a05f7b5024
nobone-test <79396632+nobone-test@users.noreply.github.com>
2025-01-08 14:34:52 +0700

    Update wazuh-misp.sh

--- modules/wazuh/wazuh-misp.sh
+++ modules/wazuh/wazuh-misp.sh
@@ -7,7 +7,7 @@ CONFIG_FILE="$SOC_DIR/wazuh-docker/single-node/config/wazuh_cluster/wazuh_manage
 
 # MISP configuration to add
 MISP_CONFIG=$(cat <<'EOF'
-<!--interation-misp-->
+<!--integration-misp-->
 <integration>
   <name>custom-misp</name>
   <group>sysmon_event1,sysmon_event3,sysmon_event6,sysmon_event7,sysmon_event_15,sysmon_event_22,syscheck</group>
@@ -30,7 +30,7 @@ if [[ ! -w "$CONFIG_FILE" ]]; then
 fi
 
 # Check if the MISP configuration already exists in the file
-if grep -q "<!--interation-misp-->" "$CONFIG_FILE"; then
+if grep -q "<!--integration-misp-->" "$CONFIG_FILE"; then
   echo "MISP configuration already exists in $CONFIG_FILE."
 else
   # Create a temporary file for the modified configuration
